313c7633e822e139650ad489e160013b54de3ff4
donggune kim <kdg96g@gmail.com>
2021-08-08 21:26:26 +0900

    config 수정

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,5 +1,8 @@
 //SERVER ROUTES
-export const USER_SERVER = 'http://localhost:5000/api/users';
-export const PRODUCT_SERVER = 'http://localhost:5000/api/products';
+// export const USER_SERVER = 'http://localhost:5000/api/users';
+export const USER_SERVER = 'http://bunnymarket.o-r.kr:5000/api/users';
+// export const PRODUCT_SERVER = 'http://localhost:5000/api/products';
+export const PRODUCT_SERVER = 'http://bunnymarket.o-r.kr:5000/api/products';
 export const IMAGE_SERVER = 'http://localhost:5000/api/images';
-export const SERVER = 'http://localhost:5000';
+// export const SERVER = 'http://localhost:5000';
+export const SERVER = 'http://bunnymarket.o-r.kr:5000';
