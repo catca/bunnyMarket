32e55b78d7043946fa67f3384719ad65cb25f8a7
donggune kim <kdg96g@gmail.com>
2021-08-24 00:28:54 +0900

    client build 추가

---
+++ client/build/precache-manifest.e8466a35e63fa7170bfe4110fba3095b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9598307bb25a04bab0211e098f5839d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34f20bb1a650e41b8a05",
+    "url": "/static/css/2.39754f85.chunk.css"
+  },
+  {
+    "revision": "3e246ac3a3a971c2e5c8",
+    "url": "/static/css/main.fadaeeb8.chunk.css"
+  },
+  {
+    "revision": "34f20bb1a650e41b8a05",
+    "url": "/static/js/2.45e23ca7.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.45e23ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e246ac3a3a971c2e5c8",
+    "url": "/static/js/main.e5045b63.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "38c81d5e8bad31c37c80d10df2bb906f",
+    "url": "/static/media/NotoSansKR-Bold.38c81d5e.otf"
+  },
+  {
+    "revision": "5fc6f1266e7ea70d652bbdd2ec3a60a8",
+    "url": "/static/media/NotoSansKR-Bold.5fc6f126.woff"
+  },
+  {
+    "revision": "d197a963b8be534ec3620e51addcf533",
+    "url": "/static/media/NotoSansKR-Bold.d197a963.woff2"
+  },
+  {
+    "revision": "4af942da40f1971ac1faa4a27deae858",
+    "url": "/static/media/NotoSansKR-Light.4af942da.otf"
+  },
+  {
+    "revision": "87f51d248fafffc79156ea28f24f1979",
+    "url": "/static/media/NotoSansKR-Light.87f51d24.woff"
+  },
+  {
+    "revision": "9ea09379fe14e88d01db5ddb4c2814d8",
+    "url": "/static/media/NotoSansKR-Light.9ea09379.woff2"
+  },
+  {
+    "revision": "bfaf1857e71d0b18bac8956117b642df",
+    "url": "/static/media/NotoSansKR-Medium.bfaf1857.woff2"
+  },
+  {
+    "revision": "e68e938a2d16e7b9fce4f484e045dc6e",
+    "url": "/static/media/NotoSansKR-Medium.e68e938a.otf"
+  },
+  {
+    "revision": "f3b73884150a09ab88bc6954a6e24758",
+    "url": "/static/media/NotoSansKR-Medium.f3b73884.woff"
+  },
+  {
+    "revision": "af8df214e02587ffb3b131fda847e87d",
+    "url": "/static/media/NotoSansKR-Regular.af8df214.woff"
+  },
+  {
+    "revision": "c3f8d68fe959e359dbf56d591b7865b2",
+    "url": "/static/media/NotoSansKR-Regular.c3f8d68f.otf"
+  },
+  {
+    "revision": "f97524d1b3a0c3b3943493627b7a4fc6",
+    "url": "/static/media/NotoSansKR-Regular.f97524d1.woff2"
+  }
+]);
